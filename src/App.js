1961605f625639510ce4179a5d544c80378c5c74
MakerJoseph <1joelmaker@gmail.com>
2020-05-08 04:45:24 +0300

    added the ToDo form

--- src/App.js
+++ src/App.js
@@ -2,27 +2,39 @@ import React, { useState } from 'react';
 import './App.css'; 
 
 function Todo (todo, index ) {  
-  return (
-    <div className='todo'> 
-      {todo.text} 
-    </div>
-  )
-  
+  return <div className='todo'> { todo.text } </div>;
 }
 
-function App() {
-  const [todos, setTodo ] = useState([
+function TofoForm ({addTodo}) {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
+  }
+
+  return(
+    <form onSubmit={handleSubmit}>
+      <input type="text" className='input' value={value} onChange={e => setValue(e.target.value)}/>
+    </form>
+  )
+}
+
+function App() { 
+  const [todos, setTodos ] = useState([
     {
-      text: 'learn about react',
-      isCompleted: false  //check boolean boolean
+      text: "learn about react",
+      isCompleted: false 
     },
     {
       text: 'build toDo app',
-      isCompleted: false  //check boolean boolean
+      isCompleted: false 
     },
     {
       text: 'another Todo',
-      isCompleted: false  //check boolean boolean
+      isCompleted: false 
     }
   ]);
 
